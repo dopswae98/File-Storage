cc97a3219c8a0a9abef41f508868881294bdc396
dopswae98 <ephraimmatarutse@gmail.com>
2024-01-17 23:46:29 +0200

    'added'

---
+++ vfm/vfm-admin/_content/users/users.php
@@ -0,0 +1,27 @@
+<?php
+
+ $_USERS = array (
+  0 => 
+  array (
+    'name' => 'admin',
+    'pass' => '$1$CcwM2YDJ$hnNyZ74xZSMsFQfzospOz0',
+    'role' => 'superadmin',
+  ),
+  1 => 
+  array (
+    'name' => 'Student',
+    'pass' => '$1$dynUTjYh$VMRtvTL4t7yq6ftITK/af/',
+    'role' => 'user',
+    'disabled' => false,
+    'dir' => '["Private","Student"]',
+    'email' => 'student@gmail.com',
+  ),
+  2 => 
+  array (
+    'name' => 'master',
+    'pass' => '$1$ephKRv6O$rRT8N8qYOx26Pcma6P0GD1',
+    'role' => 'superadmin',
+    'disabled' => false,
+    'email' => 'master@gmail.com',
+  ),
+);
